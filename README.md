7a6a1a64f1e8105edcf9ebeb2dd6b4b8d9b3678e
João Tavares <joaostavares@outlook.com>
2022-10-15 06:02:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+A simple web page to add my personal links in a simple way.
+
+[I have some other ideas to apply to this project in my spare time]
+
+### Para executar localmente
+
+Os comandos em ordem:
+
+```bash
+npm install
+# or
+yarn install
+````
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) in your browser of choice.
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>João Tavares</title>
+        <meta name="description" content="João Tavares" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+          <img className={styles.profile} src="/profile.png" />
+          <h3 className={styles.description}>João Tavares</h3>
+          <div className={styles.grid}>
+            <a href="https://github.com/joaostavares" className={styles.card} target="_blank" rel="noopener noreferrer">
+              <h3>Github</h3>
+            </a>
+            <a href="https://www.linkedin.com/in/joaotavaress/" className={styles.card} target="_blank" rel="noopener noreferrer">
+              <h3>LinkedIn</h3>
+            </a>
+            <a href="https://www.instagram.com/joao.tavares_9/" className={styles.card} target="_blank" rel="noopener noreferrer">
+              <h3>Instagram</h3>
+            </a>
+            <a href="https://steamcommunity.com/id/werewolf-gg/" className={styles.card} target="_blank" rel="noopener noreferrer">
+              <h3>Steam</h3>
+            </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+          {/*todo*/}
+      </footer>
+    </div>
+  )
+}
