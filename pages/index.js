e9967513e0e79f43fd354caa17659b962056c9c1
João Tavares <joaostavares@outlook.com>
2022-11-02 23:19:19 -0300

    Temp Changes

--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,118 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import profilePic from '../public/profile.png'
+import bgImage from '../public/background.png'
+import {motion} from 'framer-motion';
+
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>João Tavares</title>
-        <meta name="description" content="João Tavares" />
-        <link rel="icon" href="favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>João Tavares</title>
+                <meta name="description" content="João Tavares"/>
+                <link rel="icon" href="favicon.ico"/>
+            </Head>
 
-      <main className={styles.main}>
-          <Image
-            src={profilePic}
-            alt="Profile Picture"
-            height={200}
-            width={200}
-            className={styles.profile}
-            priority
-            placeholder="blur"
-          />
-          <h3 className={styles.description}>João Tavares</h3>
-          <div className={styles.grid}>
-            <a href="https://github.com/joaostavares" className={styles.card} target="_blank" rel="noopener noreferrer">
-              <h3>Github</h3>
-            </a>
-            <a href="https://www.linkedin.com/in/joaotavaress/" className={styles.card} target="_blank" rel="noopener noreferrer">
-              <h3>LinkedIn</h3>
-            </a>
-            <a href="https://www.instagram.com/joao.tavares_9/" className={styles.card} target="_blank" rel="noopener noreferrer">
-              <h3>Instagram</h3>
-            </a>
-            <a href="https://steamcommunity.com/id/werewolf-gg/" className={styles.card} target="_blank" rel="noopener noreferrer">
-              <h3>Steam</h3>
-            </a>
+            <main className={styles.main}>
+                <div>
+                    <div className="bg"></div>
+                </div>
+                <motion.div initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: .4
+                        }
+                    },
+                }}>
+                    <Image
+                        src={profilePic}
+                        alt="Profile Picture"
+                        height={200}
+                        width={200}
+                        className={styles.profile}
+                        priority
+                        placeholder="blur"
+                    />
+                </motion.div>
+                <motion.h3 className={styles.description}initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: .5
+                        }
+                    },
+                }}>João Tavares</motion.h3>
+                <motion.div className={styles.grid} initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: .75
+                        }
+                    },
+                }}>
+
+                    <motion.a href="https://github.com/joaostavares" className={styles.card} target="_blank"
+                              rel="noopener noreferrer" whileHover={{
+                        scale: 1.15,
+                        transition: {
+                            duration: .2
+                        },
+                        backgroundColor: '#3d3d3d',
+                        backgroundBlendMode: 'multiply',
+                    }}>
+                        <h3>Github</h3>
+                    </motion.a>
+
+                    <motion.a href="https://www.linkedin.com/in/joaotavaress/" className={styles.card} target="_blank"
+                              rel="noopener noreferrer" whileHover={{
+                        scale: 1.18,
+                        transition: {
+                            duration: .2
+                        }
+                    }}>
+                        <h3>LinkedIn</h3>
+                    </motion.a>
+                    <motion.a href="https://www.instagram.com/joao.tavares_9/" className={styles.card} target="_blank"
+                              rel="noopener noreferrer" whileHover={{
+                        scale: 1.18,
+                        transition: {
+                            duration: .2
+                        }
+                    }}>
+                        <h3>Instagram</h3>
+                    </motion.a>
+                    <motion.a href="https://steamcommunity.com/id/werewolf-gg/" className={styles.card} target="_blank"
+                              rel="noopener noreferrer" whileHover={{
+                        scale: 1.18,
+                        transition: {
+                            duration: .2
+                        }
+                    }}>
+                        <h3>Steam</h3>
+                    </motion.a>
+                </motion.div>
+            </main>
+
+            <footer className={styles.footer}>
+                {/*todo*/}
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-          {/*todo*/}
-      </footer>
-    </div>
-  )
+    )
 }
