969ca0e8e7152f0ca721b0d0dc801402395fd454
João Tavares <joaostavares@outlook.com>
2022-12-22 10:21:24 -0300

    Big Refactor in project and appearance of webpage

---
+++ components/buttons.js
@@ -0,0 +1,158 @@
+import React from "react";
+import git from "../public/github.svg"
+import insta from "../public/insta.svg";
+import linkedin from "../public/linkedin.svg"
+import steam from "../public/steam.svg"
+import Image from "next/image";
+import {motion} from "framer-motion";
+
+function Buttons() {
+    return (
+        <div className='flex justify-center items-center'>
+            <ul className='w-[244px] sm:w-[360px]'>
+                <motion.div initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: 0.7
+                        }
+                    },
+                }}>
+                    <motion.li whileHover={{
+                        scale: 1.1,
+                        transition: {
+                            duration: .2
+                        }
+                    }} className='text-xl'>
+                        <a href='https://www.github.com/joaostavares'
+                           target={'_blank'}
+                           rel={'noreferrer'}
+                           className='btn'
+                        >
+                            <Image
+                                src={git}
+                                alt='Github Logo'
+                                height={24}
+                                width={24}
+                                priority
+                            />
+                            &nbsp; Github
+                        </a>
+                    </motion.li>
+                </motion.div>
+                <motion.div initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: 0.8
+                        }
+                    },
+                }}>
+                    <motion.li whileHover={{
+                        scale: 1.1,
+                        transition: {
+                            duration: .2
+                        }
+                    }} className='text-xl'>
+                        <a href='https://www.instagram.com/joaostavares'
+                           target={'_blank'}
+                           rel={'noreferrer'}
+                           className='btn'
+                        >
+                            <Image
+                                src={insta}
+                                alt='Instagram Logo'
+                                height={24}
+                                width={24}
+                                priority
+                            />
+                            &nbsp; Instagram
+                        </a>
+                    </motion.li>
+                </motion.div>
+                <motion.div initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: 0.9
+                        }
+                    },
+                }}>
+                    <motion.li whileHover={{
+                        scale: 1.1,
+                        transition: {
+                            duration: .2
+                        }
+                    }} className='text-xl'>
+                        <a href='https://www.linkedin.com/in/joaostavaress'
+                           target={'_blank'}
+                           rel={'noreferrer'}
+                           className='btn'
+                        >
+                            <Image
+                                src={linkedin}
+                                alt='LinkedIn Logo'
+                                height={24}
+                                width={24}
+                                priority
+                            />
+                            &nbsp; LinkedIn
+                        </a>
+                    </motion.li>
+                </motion.div>
+                <motion.div initial="hidden" animate="visible" variants={{
+                    hidden: {
+                        scale: .8,
+                        opacity: 0
+                    },
+                    visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                            delay: 1
+                        }
+                    },
+                }}>
+                    <motion.li whileHover={{
+                        scale: 1.1,
+                        transition: {
+                            duration: .2
+                        }
+                    }} className='text-xl' >
+                        <a href='https://steamcommunity.com/id/werewolf-gg/'
+                           target={'_blank'}
+                           rel={'noreferrer'}
+                           className='btn'
+                        >
+                            <Image
+                                src={steam}
+                                alt='Steam Logo'
+                                height={24}
+                                width={24}
+                                priority
+                            />
+                            &nbsp; Steam
+                        </a>
+                    </motion.li>
+                </motion.div>
+            </ul>
+        </div>
+    );
+}
+
+export default Buttons
--- pages/index.js
+++ pages/index.js
@@ -1,111 +1,22 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-import profilePic from '../public/profile.png'
-import {motion} from 'framer-motion';
 import { Analytics } from '@vercel/analytics/react';
+import Profile from "../components/profile";
+import Buttons from "../components/buttons";
 
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <div className='bg-gradient-to-tr from-red-400 via-blue-300 to-green-300 h-screen overflow-auto'>
             <Head>
                 <title>João Tavares</title>
                 <meta name="description" content="João Tavares"/>
                 <link rel="icon" href="favicon.ico"/>
             </Head>
-
-            <main className={styles.main}>
-                <motion.div initial="hidden" animate="visible" variants={{
-                    hidden: {
-                        scale: .8,
-                        opacity: 0
-                    },
-                    visible: {
-                        scale: 1,
-                        opacity: 1,
-                        transition: {
-                            delay: .4
-                        }
-                    },
-                }}>
-                    <Image
-                        src={profilePic}
-                        alt="Profile Picture"
-                        height={200}
-                        width={200}
-                        className={styles.profile}
-                        priority
-                        placeholder="blur"
-                    />
-                </motion.div>
-                <motion.h3 className={styles.description} initial="hidden" animate="visible" variants={{
-                    hidden: {
-                        scale: .8,
-                        opacity: 0
-                    },
-                    visible: {
-                        scale: 1,
-                        opacity: 1,
-                        transition: {
-                            delay: .5
-                        }
-                    },
-                }}>João Tavares</motion.h3>
-                <motion.div className={styles.grid} initial="hidden" animate="visible" variants={{
-                    hidden: {
-                        scale: .8,
-                        opacity: 0
-                    },
-                    visible: {
-                        scale: 1,
-                        opacity: 1,
-                        transition: {
-                            delay: .75
-                        }
-                    },
-                }}>
-
-                    <motion.a href="https://github.com/joaostavares" className={styles.card} target="_blank"
-                              rel="noopener noreferrer" whileHover={{
-                        scale: 1.1,
-                        transition: {
-                            duration: .2
-                        }
-                    }}>
-                        <h3>Github</h3>
-                    </motion.a>
-
-                    <motion.a href="https://www.linkedin.com/in/joaotavaress/" className={styles.card} target="_blank"
-                              rel="noopener noreferrer" whileHover={{
-                        scale: 1.1,
-                        transition: {
-                            duration: .2
-                        }
-                    }}>
-                        <h3>LinkedIn</h3>
-                    </motion.a>
-                    <motion.a href="https://www.instagram.com/joaostavares/" className={styles.card} target="_blank"
-                              rel="noopener noreferrer" whileHover={{
-                        scale: 1.1,
-                        transition: {
-                            duration: .2
-                        }
-                    }}>
-                        <h3>Instagram</h3>
-                    </motion.a>
-                    <motion.a href="https://steamcommunity.com/id/werewolf-gg/" className={styles.card} target="_blank"
-                              rel="noopener noreferrer" whileHover={{
-                        scale: 1.1,
-                        transition: {
-                            duration: .2
-                        }
-                    }}>
-                        <h3>Steam</h3>
-                    </motion.a>
-                </motion.div>
+            <main>
+                <Profile/>
+                <Buttons/>
             </main>
-            <Analytics />
+            <Analytics/>
         </div>
-    )
+    );
 }
